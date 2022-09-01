f4dd72cd65249c7357e8031d930bfa28c640c870
Laur Bocse <laur.bocse27@yahoo.com>
2022-09-01 23:43:15 +0300

    add animations for header and about sections

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
-new Typewriter('.dynamic-text', {
-    strings: ['Laur Bocse.', 'a Software Developer.'],
-    autoStart: true,
-    loop: true
+document.addEventListener('DOMContentLoaded', () => {
+
+    new Typewriter('.dynamic-text', {
+        strings: ['<span class="text-yellow-500">Laur Bocse.</span>', 'a <span class="text-yellow-500">Software Developer.</span>'],
+        autoStart: true,
+        loop: true,
+        
+    });
+
+    gsap.from('#header_subtitle', {
+        delay: 0.5,
+        duration: 1,
+        autoAlpha: 0,
+        y: -50,
+        stagger: 0.4,
+    });
+
+    gsap.from('#header_button', {
+        delay: 1.5,
+        duration: 1,
+        autoAlpha: 0,
+        y: 50,
+        stagger: 0.4,
+    });
+
+    gsap.from('#header_socials', {
+        delay: 2.5,
+        duration: 1,
+        autoAlpha: 0,
+        y: 50,
+        stagger: 0.4,
+    });
+
+    const aboutSection = {
+        trigger: '#section_about',
+        start: 'top 70%',
+        end: '20% 80%',
+        scrub: false,
+        markers: false
+    };
+
+    gsap.from('#about_section', {
+        duration: 1,
+        autoAlpha: 0,
+        x: -150,
+        stagger: 0.4,
+        scrollTrigger: aboutSection
+    });
 });
-    
